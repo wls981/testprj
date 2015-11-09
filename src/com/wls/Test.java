aba41d69ca56916311c31256791e7b1901af2776
wls <wls@test.com>
2015-11-10 01:01:11 +0800

    commit2...

--- src/com/wls/Test.java
+++ src/com/wls/Test.java
@@ -3,7 +3,8 @@ package com.wls;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("my first git project...");
+		System.out.println("my first git project...1");
+		System.out.println("my first git project...2");
 	}
 
 }
