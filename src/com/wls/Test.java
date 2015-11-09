8b624d95a31ecfc6fff0dbc6ab43c83dd6e509c0
wls981 <184930942@qq.com>
2015-11-10 01:12:25 +0800

    Update Test.java

--- src/com/wls/Test.java
+++ src/com/wls/Test.java
@@ -3,7 +3,7 @@ package com.wls;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("say some thing ... 3");
+		System.out.println("say some thing ... 4");
 	}
 
 }
