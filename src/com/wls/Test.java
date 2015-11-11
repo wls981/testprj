9c5bab651042eacd32f416c14ec077e5a00e2944
wls <wls@test.com>
2015-11-11 21:41:22 +0800

    11

--- src/com/wls/Test.java
+++ src/com/wls/Test.java
@@ -2,6 +2,6 @@ package com.wls;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("say some thing ... 4");
+		System.out.println("say some thing ... 4xx");
 	}
 }
