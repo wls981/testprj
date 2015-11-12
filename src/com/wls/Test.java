0f6c86e2a8c8114435aba74363d166cdcf85b5d9
wls <wls@test.com>
2015-11-12 23:08:16 +0800

    dsfdsfd

--- src/com/wls/Test.java
+++ src/com/wls/Test.java
@@ -2,6 +2,6 @@ package com.wls;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("say some thing ... 4xx");
+		System.out.println("这边我也改");
 	}
 }
--- src/com/wls/Test2.java
+++ src/com/wls/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("my name is test2...d");
-		System.out.println("�Ҹ��˶���");
+		System.out.println("怎么乱码了呀");
 	}
 
 }
