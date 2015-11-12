880126a08dcc77f42b3946c7760332c93ddcd714
wls <wls@test.com>
2015-11-12 21:09:56 +0800

    develop

--- src/com/wls/Test2.java
+++ src/com/wls/Test2.java
@@ -3,7 +3,7 @@ package com.wls;
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("my name is test2...");
+		System.out.println("my name is test2...develop");
 	}
 
 }
